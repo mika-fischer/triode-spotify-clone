ecf360a7afdcdbc9b408fe4ea499684f4c09c816
Mika Fischer <mika.fischer@zoopnet.de>
2012-10-20 14:56:43 +0200

    Disable debugging statements

--- JSON.pm
+++ JSON.pm
@@ -2,21 +2,18 @@ package Plugins::SpotifyMika::JSON;
 
 use strict;
 use JSON::XS::VersionOneAndTwo;
-use Slim::Utils::Log;
+# use Slim::Utils::Log;
 
-my $log = logger('plugin.spotifymika');
+# my $log = logger('plugin.spotifymika');
 
 sub from_json_spotify {
     my $json = shift;
-    $log->debug("Original:");
-    $log->debug("$json");
+    #$log->debug("Original:");
+    #$log->debug("$json");
     $json =~ s/("spotify):/\1mika:/g;
-    $log->debug("Modified:");
-    $log->debug("$json");
-    my $result = from_json($json);
-    $log->debug("Result:");
-    $log->debug($result);
-    return $result;
+    #$log->debug("Modified:");
+    #$log->debug("$json");
+    return from_json($json);
 }
 
 1;
